ddcdd287e910c3e6346d4d018b2b5a830ae55d92
jpsegura884 <jpsegura884@gmail.com>
2026-06-23 16:44:27 -0400

    Add next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,16 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'whiteoakrichmond.com' },
-      { protocol: 'https', hostname: 'seal-richmond.bbb.org' },
+      {
+        protocol: 'https',
+        hostname: 'whiteoakrichmond.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'seal-richmond.bbb.org',
+      },
     ],
   },
 };
+
 module.exports = nextConfig;
